1e14afaedafd2b4276e59fba764b3fff467b234a
Anton Gudenko <antong702@gmail.com>
2024-07-31 22:42:23 +0300

    update configs

--- client/src/main.js
+++ client/src/main.js
@@ -1,14 +1,18 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import { createHead } from "@unhead/vue";
+import { autoAnimatePlugin } from "@formkit/auto-animate/vue";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(createHead());
+app.use(router);
+app.use(autoAnimatePlugin);
 
-app.mount('#app')
+app.mount("#app");
